4a335cd97c38b72f894207e2e816d996c1384957
Anuraag <anuraag.pandey2301@gmail.com>
2025-04-07 20:55:23 +0530

    After Method is and also with that Before will update

--- PractiseEmployee/PractiseEmployee/src/main/java/com/PractiseEmployee/AOP/AOPConfiguration.java
+++ PractiseEmployee/PractiseEmployee/src/main/java/com/PractiseEmployee/AOP/AOPConfiguration.java
@@ -12,9 +12,9 @@ public class AOPConfiguration {
 
     @Around("execution(* com.PractiseEmployee.Controllers.EmployeeControllers.getEmployee(..))")
     public Object beforeMethod(ProceedingJoinPoint joinPoint) throws Throwable {
-        System.out.println("⭐ Inside BeforeMethod Aspect - Executing getEmployee()");
+        System.out.println("⭐ Inside Before Method Aspect - Executing getEmployee()");
         Object result = joinPoint.proceed();
-        System.out.println("⭐ Inside AfterMethod Aspect - Executing getEmployee()");
+        System.out.println("⭐ Inside After Method Aspect - Executing getEmployee()");
         return result;
     }
 }
